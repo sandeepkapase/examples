8187e235efb749b4b11d9730d60f7bce63adb343
Sandeep Kapse <sandeep.kapase@gmail.com>
2018-08-15 16:44:41 +0530

    'Hello World' LRU implementation.
    Signed-off-by: Sandeep Kapse <sandeep.kapase@gmail.com>

---
+++ lru.h
@@ -0,0 +1,36 @@
+// lru.c
+struct cacheNode;
+typedef struct cacheNode cacheNode;
+struct cacheNode {
+    cacheNode *next, *prev;
+    int key;                 // block/page no.
+    int data;                // block/page data.
+};
+
+
+struct cacheQueue;
+typedef struct cacheQueue cacheQueue;
+struct cacheQueue {
+    int maxSize;            // ssd bloks/ram frames
+    int count;              // used
+    cacheNode *front, *rear;
+};
+
+
+struct cacheHeap;
+typedef struct cacheHeap cacheHeap;
+struct cacheHeap {
+    int capacity;           // disk bloks/page no.
+    cacheNode **arr;        // pointer array (index based on key/block
+};
+
+struct cache;
+typedef struct cache cache;
+struct cache {
+    pthread_mutex_t lock;   // concurrent access/multithreading
+    cacheQueue cQueue;      // FIFO
+    cacheHeap cHeap;        // O(1) search
+};
+
+int refereCache(cache *csh, int key);
+cache * newCache(int heapCapacity, int cacheSize);
