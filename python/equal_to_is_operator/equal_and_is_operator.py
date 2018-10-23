a4fa9d9b9974f33df9d389f82b1025c1f2725d82
Sandeep Kapse <sandeep.kapase@gmail.com>
2018-10-23 17:24:41 +0530

    python comparison and is operator info.

---
+++ python/equal_to_is_operator/equal_and_is_operator.py
@@ -0,0 +1,28 @@
+#!/usr/bin/evn python
+'''
+Summary: "==" and "is" operator in python
+========
+
+* In python each object has value and identity
+
+* Value can be compared using "==" operator.
+
+* Identity is verified using "is" operator.
+
+* Two objects can have same value but different identity.
+
+* value comparision    : "x == y"
+
+* identity comparision : "x is y"
+
+* "is" operator can be explained as shorthand for "id(x) == id(y)"
+
+* Eg.
+    >> x = 1.0
+    >> y = 1
+    >> x == y
+       True
+    >> x is y
+       False
+'''
+print "sandeep"
