6f6ed5d8d0a09f309b939ff854f6400e8c199e89
Sandeep Kapse <sandeep.kapse@druva.com>
2019-08-19 17:16:27 +0530

    parameter based topic creation and subscription for topic.

--- aws/sns/topicAdd.go
+++ aws/sns/topicAdd.go
@@ -11,7 +11,7 @@ import (
 
 const httpEndpoint = "http://ec2-100-25-10-35.compute-1.amazonaws.com"
 const httpProtocol = "http"
-const snsTopicARN = "arn:aws:sns:us-east-1:361108732765:"
+const snsTopicARN = "arn:aws:sns:us-east-1:361108732765:TestTopic"
 
 var awsRegion = "us-east-1"
 var endpoint = ""
@@ -20,20 +20,13 @@ var ak = os.Getenv("AWS_ACCESS_KEY_ID")
 var sk = os.Getenv("AWS_SECRET_ACCESS_KEY")
 
 //subscribe sends a subscribe request to SNS topic and initiates the subscritption process
-func subscribe(endPoint string, protocol string, topicARN string) {
+func topicAdd(topicName string) {
 
 	cfg := aws.Config{}
 	cfg.Region = &awsRegion
 	cfg.Endpoint = &endpoint
 	cfg.Credentials = credentials.NewStaticCredentials(ak, sk, "")
 
-	input := &sns.SubscribeInput{
-		Endpoint: &endPoint,
-		Protocol: &protocol,
-		TopicArn: &topicARN,
-	}
-
-	//sess, err := session.NewSession(&aws.Config{Region: aws.String(getenv("AWS_REGION", awsRegion))})
 	sess, err := session.NewSession(&cfg)
 	if err != nil {
 		fmt.Printf("Unable to initiate session")
@@ -41,12 +34,12 @@ func subscribe(endPoint string, protocol string, topicARN string) {
 	}
 	svc := sns.New(sess)
 
-	out, err := svc.Subscribe(input)
+	resp, err := svc.CreateTopic(&sns.CreateTopicInput{Name: &topicName})
 	if err != nil {
-		fmt.Println("Unable to Subscribe")
+		fmt.Println("Unable to create Topic: ", err)
 		return
 	}
-	fmt.Printf(*out.SubscriptionArn)
+	fmt.Printf("Resp: ", resp)
 }
 
 //getenv returns env varibale if set otherwise returns default value
@@ -59,15 +52,10 @@ func getenv(key, defaultValue string) string {
 }
 
 func main() {
-	httpEndpoint := getenv("HTTP_ENDPOINT", httpEndpoint)
-	httpProtocol := getenv("HTTP_PROTOCOL", httpProtocol)
-
 	if len(os.Args) != 2 {
 		fmt.Println("Topic name expected")
 		return
 	}
 	fmt.Println(os.Args[1])
-	snsTopicARN := getenv("SNS_TOPIC_ARN", snsTopicARN+os.Args[1])
-
-	subscribe(httpEndpoint, httpProtocol, snsTopicARN)
+	topicAdd(os.Args[1])
 }
