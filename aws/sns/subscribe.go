e26c63c7d7b97a7b6e5b7e4b9b09965baa119bf0
Sandeep Kapse <sandeep.kapse@druva.com>
2019-08-19 16:28:59 +0530

    sns subscript to topic and confirm subscription.

---
+++ aws/sns/subscribe.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/sns"
+	"os"
+)
+
+const httpEndpoint = "http://ec2-100-25-10-35.compute-1.amazonaws.com"
+const httpProtocol = "http"
+const snsTopicARN = "arn:aws:sns:us-east-1:361108732765:TestTopic"
+
+var awsRegion = "us-east-1"
+var endpoint = ""
+
+var ak = os.Getenv("AWS_ACCESS_KEY_ID")
+var sk = os.Getenv("AWS_SECRET_ACCESS_KEY")
+
+//subscribe sends a subscribe request to SNS topic and initiates the subscritption process
+func subscribe(endPoint string, protocol string, topicARN string) {
+
+	cfg := aws.Config{}
+	cfg.Region = &awsRegion
+	cfg.Endpoint = &endpoint
+	cfg.Credentials = credentials.NewStaticCredentials(ak, sk, "")
+
+	input := &sns.SubscribeInput{
+		Endpoint: &endPoint,
+		Protocol: &protocol,
+		TopicArn: &topicARN,
+	}
+
+	//sess, err := session.NewSession(&aws.Config{Region: aws.String(getenv("AWS_REGION", awsRegion))})
+	sess, err := session.NewSession(&cfg)
+	if err != nil {
+		fmt.Printf("Unable to initiate session")
+		return
+	}
+	svc := sns.New(sess)
+
+	out, err := svc.Subscribe(input)
+	if err != nil {
+		fmt.Println("Unable to Subscribe")
+		return
+	}
+	fmt.Printf(*out.SubscriptionArn)
+}
+
+//getenv returns env varibale if set otherwise returns default value
+func getenv(key, defaultValue string) string {
+	value := os.Getenv(key)
+	if len(value) == 0 {
+		return defaultValue
+	}
+	return value
+}
+
+func main() {
+	httpEndpoint := getenv("HTTP_ENDPOINT", httpEndpoint)
+	httpProtocol := getenv("HTTP_PROTOCOL", httpProtocol)
+	snsTopicARN := getenv("SNS_TOPIC_ARN", snsTopicARN)
+	subscribe(httpEndpoint, httpProtocol, snsTopicARN)
+}
---
+++ aws/sns/subscriber.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const subConfrmType = "SubscriptionConfirmation"
+const notificationType = "Notification"
+
+//confirmSubscription Confirms Subscription by makeing get request to Subscribe URL
+func confirmSubscription(subcribeURL string) {
+	response, err := http.Get(subcribeURL)
+	if err != nil {
+		fmt.Printf("Unbale to confirm subscriptions")
+	} else {
+		fmt.Printf("Subscription Confirmed sucessfully. %d", response.StatusCode)
+	}
+
+}
+
+//handler processes messages sent by SNS
+func handler(w http.ResponseWriter, r *http.Request) {
+	var f interface{}
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("Unable to Parse Body")
+	}
+	fmt.Printf(string(body))
+	err = json.Unmarshal(body, &f)
+	if err != nil {
+		fmt.Printf("Unable to Unmarshal request")
+	}
+
+	data := f.(map[string]interface{})
+	fmt.Println(data["Type"].(string))
+
+	if data["Type"].(string) == subConfrmType {
+		subcribeURL := data["SubscribeURL"].(string)
+		go confirmSubscription(subcribeURL)
+	} else if data["Type"].(string) == notificationType {
+		fmt.Println("Recieved this message : ", data["Message"].(string))
+	}
+
+	fmt.Fprintf(w, "Sucess")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":80", nil)
+}
