2cb8f574a3337d7161615cf7ccbd335acf4af8c4
Sandeep Kapse <sandeep.kapse@druva.com>
2020-05-09 17:17:12 +0530

    go array slicing example.

---
+++ golang/slice_array_map_etc/array_slicing.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+/*
+array have two properties:
+length = len(arr) ==> how many elementes are there in array
+capacity = len(arr) ==> capacity of array
+eg.
+ arr := []int{2, 3, 5, 7, 11, 13}
+
+slicing required two input 'x' and 'y' numbers
+Eg. arr[x:y]
+    Resultant capacity = existing capacity - x
+    Resultant length   = y
+    Conditions ==> y > x always, y should be greated current capacity of arr
+    resultant array will start point to 'current pointer' + x
+
+   arr[0:4] ==> {2, 3, 5, 7}
+   arr[2:4] ==> {5, 7, 11, 13}
+*/
+
+func main() {
+	s := []int{2, 3, 5, 7, 11, 13}
+	printSlice(s)
+
+	// Slice the slice to give it zero length.
+	s = s[:0]
+	printSlice(s)
+
+	// Extend its length.
+	s = s[:4]
+	printSlice(s)
+
+	// Drop its first two values.
+	s = s[2:6]
+	printSlice(s)
+
+	// Drop its first two values.
+	s = s[:4]
+	printSlice(s)
+
+}
+
+func printSlice(s []int) {
+	fmt.Printf("len=%d cap=%d %v\n", len(s), cap(s), s)
+}
